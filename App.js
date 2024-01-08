476ac6d9274ddb580f22ca0fff46bfcb81a115b0
unknown <sakarim9124@gmail.com>
2024-01-08 11:28:53 +0530

    updated

--- App.js
+++ App.js
@@ -1,16 +1,33 @@
-import { View } from "react-native";
-
+import { ImageBackground, StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 //comp
 // import GoalApp from "./component/goalApp";
 import StartGameScreen from "./screens/StartGameScreen";
 
 const App = () => {
   return (
-    <>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       {/* <GoalApp /> */}
-      <StartGameScreen />
-    </>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    // backgroundColor: "#ddb52f",
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.3,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     marginTop: 100,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     //android only for boxShadow ( elevation )
     elevation: 8,
