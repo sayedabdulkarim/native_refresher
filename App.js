4ab70ba78138e631b9866502af0fc872196bdb25
unknown <sakarim9124@gmail.com>
2024-01-11 22:50:27 +0530

    updated

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 //Apps
 // import TargetApp from "./component/targetApp";
-import MealApp from "./screens/mealsApp";
+// import MealApp from "./screens/mealsApp";
+import DrawerApp from "./screens/drawerApp";
+
+/**
+ * for Meal App change bgColor to #24180f in app.json
+ */
 
 const App = () => {
   return (
     <View style={styles.rootScreen}>
       {/* <TargetApp /> */}
-      <MealApp />
-      {/* <Text>Hello world</Text> */}
+      {/* <MealApp /> */}
+      <DrawerApp />
     </View>
   );
 };
---
+++ screens/drawerApp/index.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
